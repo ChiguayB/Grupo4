9803207db68b0893ab04557dcffa6a20fc41dd83
BrandonC <chiguay.brandond@gmail.com>
2023-04-15 00:20:46 -0300

    servicios contenido

--- Servicios/servicios.js
+++ Servicios/servicios.js
@@ -1,14 +1,17 @@
 function myFunction() {
     var x = document.getElementById("myLinks");
     var y = document.getElementById("nav")
-    var z = document.getElementById("myLinks")
+    var z = document.getElementById("hambur-off")
     
     if (x.style.display === "flex") {
       x.style.display = "none";
       y.className = "menu-desktop"
+      z.className = "hambur-active"
+
     } else {
       x.style.display = "flex";
       y.className = "menu-mobile"
+      z.className = "hambur-off"
     }
 
   }
